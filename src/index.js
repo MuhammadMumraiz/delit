9521ff1ca5f5e11d923627a3ae31cce6e137cf64
mumraizMalik <116867024+mumraizMalik@users.noreply.github.com>
2022-11-17 17:18:53 +0500

    wqq

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-const cors =require("cors")
-const app = require('./server');
-const server= require('http').createServer(app)
-const express = require("express");
-const io = require("socket.io")(5000);
+// const cors =require("cors")
+// const app = require('./server');
+// const express = require("express");
+// const server= require('http').createServer(express)
+
+
+// const port=process.env.PORT || 5000
+
+// const io = require("socket.io")(server);
 
 
 
-
-
+// server.listen(PORT, ()=>{
+//   console.log("Application running successfully on port: "+PORT);
+// });
 
 // io.on("connection", (socket) => {
 //     socket.emit("HelloWorld")
@@ -36,6 +41,46 @@ const io = require("socket.io")(5000);
 //     res.json({msg:"HSSSSi"})
 // })
 
+// io.on("connection", (socket) => {
+//     socket.emit("hello", 1, "2", { 3: '4', 5: Buffer.from([6]) });
+//   });
+
+
+var cors = require('cors')
+// Setup basic express server
+const express = require('express');
+const app = express();
+const path = require('path');
+const server = require('http').createServer(app);
+
+// const port = process.env.PORT || 5000;
+const port =  5000;
+
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+server.listen(port, () => {
+  console.log('Server listening at port %d', port);
+  
+});
+
 io.on("connection", (socket) => {
-    socket.emit("hello", 1, "2", { 3: '4', 5: Buffer.from([6]) });
-  });
+  socket.emit("hello", 1, "2", { 3: '4', 5: Buffer.from([6]) });
+
+  socket.on("ChatMsg",(message)=>{
+    console.log(message)
+    socket.emit("reciMsg",message)
+  })
+
+  
+
+});
+
+
+
+    
